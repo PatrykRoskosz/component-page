7d8264c6876c8f16b2896f02e547e12c3fbd4e3f
PatrykRoskosz <roskoszpatryk55@gmail.com>
2023-05-18 19:53:47 +0200

    add react-icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Button from "./components/Button/Button";
 
 function App() {
- 
 	return (
 		<>
 			<div>App</div>
-      <Button primary secondary>Hej</Button>
+			<Button success rounded outline>
+				kliknij mnie
+			</Button>
+			<Button danger outline onClick={()=> console.log('hej')}>
+				kliknij mnie
+			</Button>
+			<Button warning>kliknij mnie</Button>
+			<Button secondary outline>
+				kliknij mnie
+			</Button>
+			<Button primary rounded>
+				kliknij mnie
+			</Button>
 		</>
 	);
 }
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,4 +1,4 @@
-import propTypes from "prop-types";
+import className from 'classnames';
 
 const Button = ({
 	children,
@@ -9,8 +9,24 @@ const Button = ({
 	danger,
 	outline,
 	rounded,
+	onClick
 }) => {
-	return <button className="px-3 py-1.5 border border-blue-600 text-white bg-blue-500">{children}</button>;
+
+	const classes = className('px-3 py-1.5 border', {
+		'border-blue-500 bg-blue-500 text-white': primary,
+		'border-gray-900 bg-gray-900 text-white': secondary,
+		'border-green-500 bg-green-500 text-white': success,
+		'border-yellow-400 bg-yellow-400 text-white': warning,
+		'border-red-500 bg-red-600 text-white': danger,
+		'rounded-full': rounded,
+		'bg-white border-2': outline,
+		'text-blue-500': outline && primary,
+		'text-gray-900': outline && secondary,
+		'text-green-500': outline && success,
+		'text-yellow-400': outline && warning,
+		'text-red-500': outline && danger,
+	})
+	return <button className={classes} onClick={onClick}>{children}</button>;
 };
 
 Button.propTypes = {
