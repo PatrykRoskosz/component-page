eeeb1d2dfc29e69144513e19bbdc25550a47fb18
PatrykRoskosz <roskoszpatryk55@gmail.com>
2023-05-18 11:22:37 +0200

    add libraries (classnames and taliwind)

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -10,7 +10,7 @@ const Button = ({
 	outline,
 	rounded,
 }) => {
-	return <button>{children}</button>;
+	return <button className="px-3 py-1.5 border border-blue-600 text-white bg-blue-500">{children}</button>;
 };
 
 Button.propTypes = {
