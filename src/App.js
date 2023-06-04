96eabfb5701fb873131b7192ad5ef44742af0920
PatrykRoskosz <roskoszpatryk55@gmail.com>
2023-06-04 09:27:59 +0200

    create Link component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-
+import Link from "./components/Link/Link";
 
 function App() {
 	
 	return (
-	<></>
+	<div>
+		<Link to='/accordion'>Go to accordion</Link>
+		<br/>
+		<Link to='/dropdown'>Go to dropdown</Link> 
+	</div>
 	)
 }
 
